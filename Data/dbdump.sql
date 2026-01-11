f50eb0ef4f860a7586aaea0ca2e50db4934789cc
hungpt06013 <hungpt0604@gmail.com>
2026-01-11 17:10:10 +0700

    Update dbdump.sql

--- Data/dbdump.sql
+++ Data/dbdump.sql
@@ -4,28 +4,28 @@ CREATE TABLE "admin_settings" (
 	"setting_key" varchar(100) NOT NULL CONSTRAINT "admin_settings_setting_key_key" UNIQUE,
 	"prompt_template" text,
 	"json_format_response" text,
-	"updated_at" timestamp DEFAULT CURRENT_TIMESTAMP,
+	"updated_at" timestamp DEFAULT (now() AT TIME ZONE 'Asia/Ho_Chi_Minh'::text),
 	"updated_by" integer,
 	"manual_prompt_template" text
 );
 CREATE TABLE "ai_query_history" (
 	"id" serial PRIMARY KEY,
 	"user_id" integer NOT NULL,
-	"query_time" timestamp DEFAULT CURRENT_TIMESTAMP,
+	"query_time" timestamp DEFAULT (now() AT TIME ZONE 'Asia/Ho_Chi_Minh'::text),
 	"prompt_content" text NOT NULL,
 	"status" varchar(20) DEFAULT 'PENDING',
 	"roadmap_id" integer,
 	"error_message" text,
 	"response_tokens" integer,
-	"created_at" timestamp DEFAULT CURRENT_TIMESTAMP,
-	"updated_at" timestamp DEFAULT CURRENT_TIMESTAMP,
+	"created_at" timestamp DEFAULT (now() AT TIME ZONE 'Asia/Ho_Chi_Minh'::text),
+	"updated_at" timestamp DEFAULT (now() AT TIME ZONE 'Asia/Ho_Chi_Minh'::text),
 	CONSTRAINT "ai_query_history_status_check" CHECK (CHECK (((status)::text = ANY ((ARRAY['PENDING'::character varying, 'SUCCESS'::character varying, 'FAIL'::character varying, 'TIMEOUT'::character varying])::text[]))))
 );
 CREATE TABLE "categories" (
 	"id" serial PRIMARY KEY,
 	"name" varchar(100) NOT NULL CONSTRAINT "categories_name_key" UNIQUE,
 	"description" text,
-	"created_at" timestamp DEFAULT CURRENT_TIMESTAMP
+	"created_at" timestamp DEFAULT (now() AT TIME ZONE 'Asia/Ho_Chi_Minh'::text)
 );
 CREATE TABLE "learning_roadmap_details" (
 	"detail_id" serial PRIMARY KEY,
@@ -38,8 +38,8 @@ CREATE TABLE "learning_roadmap_details" (
 	"study_duration" numeric(4, 2) NOT NULL,
 	"completion_status" varchar(20) DEFAULT 'NOT_STARTED',
 	"study_date" date,
-	"created_at" timestamp DEFAULT CURRENT_TIMESTAMP,
-	"updated_at" timestamp DEFAULT CURRENT_TIMESTAMP,
+	"created_at" timestamp DEFAULT (now() AT TIME ZONE 'Asia/Ho_Chi_Minh'::text),
+	"updated_at" timestamp DEFAULT (now() AT TIME ZONE 'Asia/Ho_Chi_Minh'::text),
 	"completed_at" timestamp,
 	"usage_instructions" text,
 	CONSTRAINT "learning_roadmap_details_roadmap_id_day_number_key" UNIQUE("roadmap_id","day_number"),
@@ -58,8 +58,8 @@ CREATE TABLE "learning_roadmap_details_system" (
 	"usage_instructions" text,
 	"study_duration" numeric(4, 2),
 	"completion_status" varchar(20),
-	"created_at" timestamp DEFAULT now(),
-	"updated_at" timestamp DEFAULT now(),
+	"created_at" timestamp DEFAULT (now() AT TIME ZONE 'Asia/Ho_Chi_Minh'::text),
+	"updated_at" timestamp DEFAULT (now() AT TIME ZONE 'Asia/Ho_Chi_Minh'::text),
 	"completed_at" timestamp
 );
 CREATE TABLE "learning_roadmaps" (
@@ -81,11 +81,10 @@ CREATE TABLE "learning_roadmaps" (
 	"content_relevance" integer,
 	"engagement_level" integer,
 	"would_recommend" boolean,
-	"created_at" timestamp DEFAULT CURRENT_TIMESTAMP,
-	"updated_at" timestamp DEFAULT CURRENT_TIMESTAMP,
+	"created_at" timestamp DEFAULT (now() AT TIME ZONE 'Asia/Ho_Chi_Minh'::text),
+	"updated_at" timestamp DEFAULT (now() AT TIME ZONE 'Asia/Ho_Chi_Minh'::text),
 	"roadmap_analyst" text,
 	"detailed_feedback" text,
-	"recommended_category" varchar(100),
 	"actual_learning_outcomes" text,
 	"improvement_suggestions" text,
 	CONSTRAINT "learning_roadmaps_content_relevance_check" CHECK (CHECK (((content_relevance >= 1) AND (content_relevance <= 5)))),
@@ -110,8 +109,8 @@ CREATE TABLE "learning_roadmaps_system" (
 	"duration_hours" numeric(5, 2),
 	"overall_rating" integer,
 	"learning_effectiveness" integer,
-	"created_at" timestamp DEFAULT now(),
-	"updated_at" timestamp DEFAULT now(),
+	"created_at" timestamp DEFAULT (now() AT TIME ZONE 'Asia/Ho_Chi_Minh'::text),
+	"updated_at" timestamp DEFAULT (now() AT TIME ZONE 'Asia/Ho_Chi_Minh'::text),
 	"roadmap_analyst" text,
 	CONSTRAINT "learning_roadmaps_system_learning_effectiveness_check" CHECK (CHECK (((learning_effectiveness >= 0) AND (learning_effectiveness <= 100)))),
 	CONSTRAINT "learning_roadmaps_system_overall_rating_check" CHECK (CHECK (((overall_rating >= 0) AND (overall_rating <= 100))))
@@ -122,14 +121,14 @@ CREATE TABLE "password_reset_codes" (
 	"code" varchar(6) NOT NULL,
 	"expires_at" timestamp NOT NULL,
 	"used" boolean DEFAULT false,
-	"created_at" timestamp DEFAULT CURRENT_TIMESTAMP
+	"created_at" timestamp DEFAULT (now() AT TIME ZONE 'Asia/Ho_Chi_Minh'::text)
 );
 CREATE TABLE "sub_categories" (
 	"id" serial PRIMARY KEY,
 	"category_id" integer NOT NULL UNIQUE,
 	"name" varchar(100) NOT NULL UNIQUE,
 	"description" text,
-	"created_at" timestamp DEFAULT CURRENT_TIMESTAMP,
+	"created_at" timestamp DEFAULT (now() AT TIME ZONE 'Asia/Ho_Chi_Minh'::text),
 	CONSTRAINT "sub_categories_category_id_name_key" UNIQUE("category_id","name")
 );
 CREATE TABLE "user_feedback" (
@@ -146,7 +145,7 @@ CREATE TABLE "user_feedback" (
 	"question_1" text,
 	"question_2" text,
 	"question_3" text,
-	"created_at" timestamp DEFAULT CURRENT_TIMESTAMP,
+	"created_at" timestamp DEFAULT (now() AT TIME ZONE 'Asia/Ho_Chi_Minh'::text),
 	CONSTRAINT "user_feedback_rating_1_check" CHECK (CHECK (((rating_1 >= 1) AND (rating_1 <= 5)))),
 	CONSTRAINT "user_feedback_rating_2_check" CHECK (CHECK (((rating_2 >= 1) AND (rating_2 <= 5)))),
 	CONSTRAINT "user_feedback_rating_3_check" CHECK (CHECK (((rating_3 >= 1) AND (rating_3 <= 5)))),
@@ -163,7 +162,7 @@ CREATE TABLE "users" (
 	"email" text NOT NULL CONSTRAINT "users_email_key" UNIQUE,
 	"password" text NOT NULL,
 	"role" text DEFAULT 'user',
-	"created_at" timestamp DEFAULT CURRENT_TIMESTAMP
+	"created_at" timestamp DEFAULT (now() AT TIME ZONE 'Asia/Ho_Chi_Minh'::text)
 );
 ALTER TABLE "admin_settings" ADD CONSTRAINT "admin_settings_updated_by_fkey" FOREIGN KEY ("updated_by") REFERENCES "users"("id") ON DELETE SET NULL;
 ALTER TABLE "ai_query_history" ADD CONSTRAINT "ai_query_history_roadmap_id_fkey" FOREIGN KEY ("roadmap_id") REFERENCES "learning_roadmaps"("roadmap_id") ON DELETE SET NULL;
